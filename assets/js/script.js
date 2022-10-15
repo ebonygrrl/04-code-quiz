9befd13db7aca0a5b33ba5c0a07411d4fb7cf1f8
Aletoria Booker <abooker77@yahoo.com>
2022-10-14 19:15:56 -0500

    stuck on for loop

--- assets/js/script.js
+++ assets/js/script.js
@@ -22,77 +22,57 @@ function timerText() {
 }
 
 function startQuiz() {
-    var items   = [
+    var item   = [
         {
             question: "Commonly used data types DO NOT include:",
-            answers: ["1. Strings", "2. Booleans", "3. Alerts", "4. Numbers"],
-            anskey: "2. Booleans"
+            options: ["strings", "booleans", "alerts", "numbers"],
+            anskey: 2
         },
         {
             question: "The condition in an if/else statement is enclosed within ______.",
-            answers: ["1. quotes", "2. curly brackets", "3. parenthesis", "4. square brackets"],
-            anskey: "3. parenthesis"
+            options: ["quotes", "curly brackets", "parenthesis", "square brackets"],
+            anskey: 3
         },
         {
             question: "Arrays in JavaScript can be used to store ______.",
-            answers: ["1. numbers and strings", "2. other arrays", "3. booleans", "4. all of the above"],
-            anskey: "4. all of the above"
+            options: ["numbers and strings", "other arrays", "booleans", "all of the above"],
+            anskey: 4
         },
         {
-            question: "String values must be enclosed within ______ when being assigned to variables.",
-            answers: ["1. commas", "2. curly brackets", "3. quotes", "4. parenthesis"],
-            anskey: "3. quotes"
+            question: "String values must be enclosed within when being assigned to variables.",
+            options: ["commas", "curly brackets", "quotes", "parenthesis"],
+            anskey: 3
         },
         {
             question: "A very usefull tool used during development and debugging for printing content to the debugger is:",
-            answers: ["1. JavaScript", "2. terminal/bash", "3. for loops", "4. console.log"],
-            anskey: "4. console.log"
-        }
+            options: ["JavaScript", "terminal/bash", "for loops", "console.log"],
+            anskey: 4
+        },
     ],
-    questionEl = "",
+    questionEl = "<h2>" + item.question + "</h2>",
     answerEl   = "";
 
-    for(i=0; i < items.length; i++ ) {
-        var item = items[i];
-        //console.log(item.answers);
-
-        newText.innerHTML = "<h2>" + items[i].question + "</h2>";
-
-        for(var key in item.answers) {
-            var answer = item.answers[key];
-            console.log(answer);
-            newText.innerHTML += "<button class='answer'>" + answer + "</button>";
-        }
-    }   
-
-    //console.log(items[0].answers.length);
-    /*
-    for (var key in item) {
-        var arr = item[key];
-        for (i=0; i < item.length; i++) {     
-            var obj = arr[i];
-            for (var prop in obj) {   
-                console.log(i);
-                //answerEl += "<button class='answer'>" + item.answers[i]+ "</button>";
-            }
-        }
+    for (i=0; i < item[i].answers.length; i++) {
+        answerEl += "<button class='answer'>" + item.answers[i]+ "</button>";
     }
 
     newText.innerHTML = questionEl;
-    newText.innerHTML += answerEl;    */
-/*
+    newText.innerHTML += answerEl;    
+
     var answerBtn = document.querySelector(".answer");
 
     answerBtn.addEventListener("click", function() {
         console.log(item["answers"]);
+/*
         if (this.item[answers] === item.key) {            
             document.getElementById("validator").innerHTML = "Correct!";
             console.log("Correct");
         } else {
             document.getElementById("validator").innerHTML = "Wrong!";            
             console.log("Wrong");
+        }*/
+
     });
-*/
 }
 //console.log(newText.innerHTML + "after");
 
