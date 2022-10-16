cb29715a9b054cb75db9a9bbc2031a6bb3d1abf7
Aletoria Booker <abooker77@yahoo.com>
2022-10-15 21:53:06 -0500

    single question at a time break through

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,74 +21,68 @@ function timerText() {
     }, 1000); // 1 second
 }
 
-function startQuiz(question, choice, valid) {
-    this.question = question;
-    this.choice = choice;
-    this.valid = valid;
-    return quizSection;
-}
-// store questions as array objects
-var item = [
+var item   = [
     {
         question: "Commonly used data types DO NOT include:",
         options: ["strings", "booleans", "alerts", "numbers"],
-        anskey: 2
+        anskey: 1
     },
     {
         question: "The condition in an if/else statement is enclosed within ______.",
         options: ["quotes", "curly brackets", "parenthesis", "square brackets"],
-        anskey: 3
+        anskey: 2
     },
     {
         question: "Arrays in JavaScript can be used to store ______.",
         options: ["numbers and strings", "other arrays", "booleans", "all of the above"],
-        anskey: 4
+        anskey: 3
     },
     {
         question: "String values must be enclosed within when being assigned to variables.",
         options: ["commas", "curly brackets", "quotes", "parenthesis"],
-        anskey: 3
+        anskey: 2
     },
     {
         question: "A very usefull tool used during development and debugging for printing content to the debugger is:",
         options: ["JavaScript", "terminal/bash", "for loops", "console.log"],
-        anskey: 4
+        anskey: 3
     },
 ],
 questionEl = "",
-answerEl   = "";
+answerEl   = "",
+keyEl      = "";
 
-    // console.log(item.length);
+var answerBtn = document.querySelector(".answer");
 
-    // loop through each object
+function startQuiz() {    
+
     for (i=0; i < item.length; i++) {
+        questionEl = "<h2>" + item[i].question + "</h2>"
+        for (j=0; j < item[i].options.length; j++) {
+            var option = item[i].options[j];
+            answerEl += "<button class='answer'>" + option + "</button>";
+        }
+        keyEl = item[i].anskey;
 
-        question = 
+        console.log(item[i].question);
+        console.log(option.valueOf());
+        console.log(keyEl);
 
-       /* questionEl = "<h2>" + item[i].question + "</h2>";
-
-        newText.innerHTML = questionEl;
-
-        answerEl = "<button class='answer'>" + item[i].options + "</button>";*/
-        
+        break;
     }
 
-    newText.innerHTML = questionEl;
-    newText.innerHTML += answerEl;    
+    newText.innerHTML = questionEl + "<br>" + answerEl;       
 
-    /*var answerBtn = document.querySelector(".answer");
-
-    answerBtn.addEventListener("click", function() {
+    /*answerBtn.addEventListener("click", function() {
         console.log(item["answers"]);
 
-        if (this.item[answers] === item.key) {            
+        if (item.option === item.anskey) {            
             document.getElementById("validator").innerHTML = "Correct!";
             console.log("Correct");
         } else {
             document.getElementById("validator").innerHTML = "Wrong!";            
             console.log("Wrong");
         }
-
     });*/
 }
 //console.log(newText.innerHTML + "after");
@@ -105,13 +99,27 @@ startQuizBtn.addEventListener("click", function() {
 
 
 
-/* 
-Questions
-1. Commonly used data types DO NOT include: strings, booleans, alerts, numbers
-2. The condition in an if/else statement is enclosed within ______. : quotes, curly brackets, parenthesis, square brackets
-3. Arrays in JavaScript can be used to store _____. : numbers and strings, other arrays, booleans, all of the above
-4. String values must be enclosed within when being assigned to variables. : commas, curly brackets, quotes, parenthesis
-5. A very usefull tool used during development and debugging for printing content to the debugger is: JavaScript, terminal/bash, for loops, console.log
-All done! Your final score is *22*. Enter initials: input, submit
+/* All done! Your final score is *22*. Enter initials: input, submit
 Highscores: 1. AB - 22 <go back> <clear highscores>
 */
+
+
+/*for(i=0; i < items.length; i++ ) {
+    var item = items[i];
+    //console.log(item.answers);
+
+    questionEl += "<h2>" + item.question + "</h2>";
+    //console.log("LOOK" + questionEl);
+
+    for(var answer of item.answers) {
+        //var answer = item.answers[key];
+        //console.log(answer);
+        answerEl = "<button class='answer'>" + answer + "</button>";
+    }
+    for(var answer of item.answers) {
+        //var answer = item.answers[key];
+        console.log(answer);
+       // answerEl = "<button class='answer'>" + answer + "</button>";
+    }
+    //newText.innerHTML = answerEl;
+}*/
